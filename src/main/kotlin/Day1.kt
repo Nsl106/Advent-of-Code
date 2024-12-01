a0faa4eea423c66557c2dbd94aa59c30e3cb45d7
Nolan L <nolan.s.locke@gmail.com>
2024-12-01 12:50:56 -0700

    add nice day 1 solution

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -5,22 +5,36 @@ data object Day1: Solution() {
     private val input by lazy { File(javaClass.getResource("1")?.toURI() ?: error("Missing input!")).readLines() }
 
     override fun solve(): Pair<Int, Int> {
-        var one = input.map {
-            it.split("   ").first()
-        }
+        val (first, second) = input.map { ln -> ln.split("   ").let { (a, b) -> a.toInt() to b.toInt() } }.unzip()
 
-        var two = input.map {
-            it.split("   ").last()
-        }
-
-        one = one.sortedDescending()
-        two = two.sortedDescending()
-
-        val f = one.zip(two)
-
-        val partOne = f.sumOf { abs(it.first.toInt() - it.second.toInt()) }
-        val partTwo = f.sumOf { two.count { etu -> etu == it.first } * it.first.toInt() }
+        val partOne = first.sorted().zip(second.sorted()).sumOf { abs(it.first - it.second) }
+        val partTwo = first.sumOf { n -> second.count { it == n } * n }
 
         return Pair(partOne, partTwo)
     }
+
+
+// Original speedrun solution
+
+    /*
+        override fun solve(): Pair<Int, Int> {
+            var one = input.map {
+                it.split("   ").first()
+            }
+
+            var two = input.map {
+                it.split("   ").last()
+            }
+
+            one = one.sortedDescending()
+            two = two.sortedDescending()
+
+            val f = one.zip(two)
+
+            val partOne = f.sumOf { abs(it.first.toInt() - it.second.toInt()) }
+            val partTwo = f.sumOf { two.count { etu -> etu == it.first } * it.first.toInt() }
+
+            return Pair(partOne, partTwo)
+        }
+    */
 }
