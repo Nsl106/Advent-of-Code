44e9fd8fc6af93d016521b9899016af3fd2e6692
Nolan L <nolan.s.locke@gmail.com>
2024-12-01 16:10:30 -0700

    comment day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -5,16 +5,21 @@ data object Day1: Solution() {
     private val input by lazy { File(javaClass.getResource("1")?.toURI() ?: error("Missing input!")).readLines() }
 
     override fun solve(): Pair<Int, Int> {
+        // Split the input into a List<Pair<String, String>>, then convert to List<Pair<Int, Int>>. Finally use .unzip() to convert it to two List<Int> that represent the columns.
         val (first, second) = input.map { it.split("   ").let { (a, b) -> a.toInt() to b.toInt() } }.unzip()
 
+        // Sort each list and recombine them to a List<Pair<Int, Int>> using zip(). Then take the sum of the differences of each paring.
         val partOne = first.sorted().zip(second.sorted()).sumOf { abs(it.first - it.second) }
+        // For each element in the first list, find how many times it appears in the second list and multiply by itself. Probably shouldn't count the second list each time, but oh well.
         val partTwo = first.sumOf { n -> second.count { it == n } * n }
 
         return Pair(partOne, partTwo)
     }
 
 
-// Original speedrun solution
+    // Original speedrun solution
+    // Part 1: 00:4:43
+    // Part 2: 00:7:30
 
     /*
         override fun solve(): Pair<Int, Int> {
