74124fd065693694dc4cee5bc852e6c8629fec14
Nolan L <nolan.s.locke@gmail.com>
2024-12-05 21:41:35 -0700

    add template file

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,15 @@
+import util.Util
+
+private fun main() {
+    val input = Util.loadInputLines(2024, TODO("Insert Date!"))
+
+    var p1 = 0
+    var p2 = 0
+
+    Util.printSolution(TODO("Insert Date!"), p1, p2)
+}
+
+
+// Original speedrun solution
+// Part 1: 00:00:00
+// Part 2: 00:00:00
