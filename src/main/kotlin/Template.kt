7eb555a391e1eb421b13d35fc369c4f67e1ec955
Nolan L <nolan.s.locke@gmail.com>
2024-12-07 23:27:17 -0700

    expand template

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,10 +1,22 @@
 import util.Util
 
 private fun main() {
-    val input = Util.loadInputLines(2024, TODO("Insert Date!"))
+    val demoInput = """
+    
+    """.trimIndent().lines()
+
+    val input =
+        Util.loadInputLines(2024, TODO("Insert Date!"))
+//        demoInput
 
     var p1 = 0
     var p2 = 0
+
+
+    for (ln in input) {
+
+    }
+
 
     Util.printSolution(TODO("Insert Date!"), p1, p2)
 }
